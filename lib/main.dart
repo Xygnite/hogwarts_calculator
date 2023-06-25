abb1447a55f288b98daaa5ec60f1a4ada0ac122a
xygnite <riconaxxbze@gmail.com>
2023-06-25 17:58:14 +0700

    implement function

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:loader_overlay/loader_overlay.dart';
 
 void main() {
   runApp(const MyApp());
@@ -7,39 +8,24 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Hogwarts Calculator',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const LoaderOverlay(
+        useDefaultLoading: false,
+        overlayOpacity: 0.8,
+        child: MyHomePage(title: 'Hogwarts Calculator'),
+      ),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -48,68 +34,144 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  List<Widget> _possibleCombinationWidget = [];
   int _counter = 0;
+  bool isLoading = false;
+  final TextEditingController _textController = TextEditingController();
 
-  void _incrementCounter() {
+  void _countCurrencyCombinations() {
+    context.loaderOverlay.show();
+    //reset counter
+    _counter = 0;
+    String currencyType =
+        _textController.text[_textController.text.length - 1].toLowerCase();
+    if (currencyType != "g" && currencyType != "s") {
+      return;
+    }
+
+    double? currency = double.tryParse(
+        _textController.text.substring(0, _textController.text.length - 1));
+    if (currency == null) {
+      return;
+    }
+    switch (currencyType) {
+      case "g":
+        currency = currency * 100;
+        break;
+      default:
+        break;
+    }
+    int parsedCurrency = currency.toInt();
+
+    //start of easy solution, assuming the rest of currency will be fulfilled by 1s
+    // //there are always 1 way to count it using 1s (i.e. 1g is always 100s)
+    // _counter += 1;
+    // //let's count using 5s
+    // _counter += parsedCurrency ~/ 5;
+    // //let's count using 10s
+    // _counter += parsedCurrency ~/ 10;
+    // //let's count using 25s
+    // _counter += parsedCurrency ~/ 25;
+    // //let's count using 50s
+    // _counter += parsedCurrency ~/ 50;
+    // //let's count using 1G
+    // _counter += parsedCurrency ~/ 100;
+    // // let's count using 2G
+    // _counter += parsedCurrency ~/ 200;
+
+    //end of easy solution
+    List<List<int>> possibleCombination = [];
+    for (var countFor200
+        in List<int>.generate(parsedCurrency ~/ 200 + 1, (i) => i)) {
+      for (var countFor100
+          in List<int>.generate(parsedCurrency ~/ 100 + 1, (i) => i)) {
+        for (var countFor50
+            in List<int>.generate(parsedCurrency ~/ 50 + 1, (i) => i)) {
+          for (var countFor25
+              in List<int>.generate(parsedCurrency ~/ 25 + 1, (i) => i)) {
+            for (var countFor10
+                in List<int>.generate(parsedCurrency ~/ 10 + 1, (i) => i)) {
+              for (var countFor5
+                  in List<int>.generate(parsedCurrency ~/ 5 + 1, (i) => i)) {
+                for (var countFor1
+                    in List<int>.generate(parsedCurrency + 1, (i) => i)) {
+                  if (200 * countFor200 +
+                          100 * countFor100 +
+                          50 * countFor50 +
+                          25 * countFor25 +
+                          10 * countFor10 +
+                          5 * countFor5 +
+                          1 * countFor1 ==
+                      parsedCurrency) {
+                    possibleCombination.add([
+                      countFor200,
+                      countFor100,
+                      countFor50,
+                      countFor25,
+                      countFor10,
+                      countFor5,
+                      countFor1
+                    ]);
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+    List<Widget> possibleCombinationWidget = [];
+    //render from highest currency
+    for (var element in possibleCombination.reversed) {
+      possibleCombinationWidget.add(Text(
+          "${element[0].toString()}x2G + ${element[1].toString()}x1G + ${element[2].toString()}x50s + ${element[3].toString()}x25s + ${element[4].toString()}x10s + ${element[5].toString()}x5s + ${element[6].toString()}x1s "));
+    }
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      context.loaderOverlay.hide();
+      _counter = possibleCombination.length;
+      _possibleCombinationWidget = possibleCombinationWidget;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              const Text(
+                'Input number of currency (i.e. 3.5G):',
+              ),
+              TextField(
+                controller: _textController,
+                style: Theme.of(context).textTheme.headlineMedium,
+              ),
+              const Text(
+                'Possible combinations of the total currency:',
+              ),
+              Text(
+                '$_counter',
+                style: Theme.of(context).textTheme.headlineMedium,
+              ),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: _possibleCombinationWidget,
+              ),
+            ],
+          ),
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
+        onPressed: _countCurrencyCombinations,
         tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+        child: const Icon(Icons.send),
+      ),
     );
   }
 }
